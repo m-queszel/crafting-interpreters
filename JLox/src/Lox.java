447ff2d337d562a7d21d2f46e122703e4c6b76d2
queszel <QueszelEdu@gmail.com>
2025-02-19 13:34:44 -0400

    feat: add error generation in Lox

--- JLox/src/Lox.java
+++ JLox/src/Lox.java
@@ -7,6 +7,9 @@ import java.nio.file.Paths;
 import java.util.List;
 
 public class Lox {
+
+	static boolean hadError = false;
+
 	public static void main(String[] args) throws IOException {
 		if (args.length > 1) {
 			System.out.println("Usage: jlox [script]");
@@ -23,6 +26,9 @@ public class Lox {
 		byte[] bytes = Files.readAllBytes(Paths.get(filePath));
 		// Converts byte array into a string using the default charset, then calls run
 		run(new String(bytes, Charset.defaultCharset()));
+		if (hadError) {
+			System.exit(65);
+		}
 	}
 
 	private static void runPrompt() throws IOException {
@@ -35,6 +41,7 @@ public class Lox {
 				break;
 			}
 			run(line);
+			hadError = false;
 		}
 	}
 
@@ -44,5 +51,14 @@ public class Lox {
 		for (Token token : tokens) {
 			System.out.println(token);
 		}
+	}
+
+	static void error(int line, String msg) {
+		report(line, "", msg);
+	}
+
+	private static void report(int line, String where, String msg) {
+		System.err.println("[line " + line + "] Error" + where + ": " + msg);
+		hadError = true;
 	}
 }
