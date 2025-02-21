3106896528c555b6dcc30ebb8c82fe75b99ea93d
queszel <QueszelEdu@gmail.com>
2025-02-20 21:20:42 -0400

    feat: add reserved words and identifiers to lexing

--- JLox/src/Scanner.java
+++ JLox/src/Scanner.java
@@ -6,6 +6,7 @@ import java.util.Map;
 public class Scanner {
 	private final String source;
 	private final List<Token> tokens = new ArrayList<>();
+	private static final Map<String, TokenType> keywords;
 	private int start = 0;
 	private int current = 0;
 	private int line = 1;
@@ -91,12 +92,26 @@ public class Scanner {
 			default:
 				if (isDigit(c)) {
 					number();
+				} else if (isAlpha(c)) {
+					identifier();
 				} else {
 					Lox.error(line, "Unexpected character.");
 					break;
 				}
 		}
 
+	}
+
+	private void identifier() {
+		while (isAlphaNumeric(peek())) {
+			advance();
+		}
+		String text = source.substring(start, current);
+		TokenType type = keywords.get(text);
+		if (type == null) {
+			type = TokenType.IDENTIFIER;
+		}
+		addToken(type);
 	}
 
 	// Helper method to tokenize and handl numbers
@@ -161,6 +176,14 @@ public class Scanner {
 		return source.charAt(current + 1);
 	}
 
+	private boolean isAlpha(char c) {
+		return (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') || c == '_';
+	}
+
+	private boolean isAlphaNumeric(char c) {
+		return isAlpha(c) || isDigit(c);
+	}
+
 	private boolean isDigit(char c) {
 		return c >= '0' && c <= '9';
 	}
@@ -180,5 +203,25 @@ public class Scanner {
 	private void addToken(TokenType type, Object literal) {
 		String text = source.substring(start, current);
 		tokens.add(new Token(type, text, literal, line));
+	}
+
+	static {
+		keywords = new HashMap<>();
+		keywords.put("and", TokenType.AND);
+		keywords.put("class", TokenType.CLASS);
+		keywords.put("else", TokenType.ELSE);
+		keywords.put("false", TokenType.FALSE);
+		keywords.put("for", TokenType.FOR);
+		keywords.put("fun", TokenType.FUN);
+		keywords.put("if", TokenType.IF);
+		keywords.put("nil", TokenType.NIL);
+		keywords.put("or", TokenType.OR);
+		keywords.put("print", TokenType.PRINT);
+		keywords.put("return", TokenType.RETURN);
+		keywords.put("super", TokenType.SUPER);
+		keywords.put("this", TokenType.THIS);
+		keywords.put("true", TokenType.TRUE);
+		keywords.put("var", TokenType.VAR);
+		keywords.put("while", TokenType.WHILE);
 	}
 }
