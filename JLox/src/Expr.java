0f6e6a9f951060bba468eda4f90068e7a0b38871
Queszel <QueszelEdu@gmail.com>
2025-03-04 15:06:48 -0400

    feat: add grouping, literal, and unary to syntax tree

--- JLox/src/Expr.java
+++ JLox/src/Expr.java
@@ -1,12 +1,40 @@
+import java.util.List;
+
 public abstract class Expr {
 	public static class Binary extends Expr {
-		Binary(Expr leftIn, Token operatorIn, Expr rightIn) {
-			this.left = leftIn;
-			this.operator = operatorIn;
-			this.right = rightIn;
+		Binary(Expr left, Token operator, Expr right) {
+			this.left = left;
+			this.operator = operator;
+			this.right = right;
 		}
 
 		final Expr left;
+		final Token operator;
+		final Expr right;
+	}
+
+	public static class Grouping extends Expr {
+		Grouping(Expr expression) {
+			this.expression = expression;
+		}
+
+		final Expr expression;
+	}
+
+	public static class Literal extends Expr {
+		Literal(Object value) {
+			this.value = value;
+		}
+
+		final Object value;
+	}
+
+	public static class Unary extends Expr {
+		Unary(Token operator, Expr right) {
+			this.operator = operator;
+			this.right = right;
+		}
+
 		final Token operator;
 		final Expr right;
 	}
