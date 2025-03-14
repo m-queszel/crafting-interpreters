d3faef7e95e9954cf73faf5cf82be6865bbade4d
queszel <queszeledu@gmail.com>
2025-03-14 18:11:34 -0300

    feat: Add statement functionality

--- JLox/src/Interpreter.java
+++ JLox/src/Interpreter.java
@@ -1,4 +1,17 @@
-public class Interpreter implements Expr.Visitor<Object> {
+import java.util.List;
+
+public class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
+
+	public void interpret(List<Stmt> statements) {
+		try {
+			for (Stmt statement : statements) {
+				execute(statement);
+			}
+		} catch (RuntimeError error) {
+			Lox.runtimeError(error);
+		}
+	}
+
 	@Override
 	public Object visitLiteralExpr(Expr.Literal expr) {
 		return expr.value;
@@ -75,6 +88,23 @@ public class Interpreter implements Expr.Visitor<Object> {
 		return expr.accept(this);
 	}
 
+	private void execute(Stmt stmt) {
+		stmt.accept(this);
+	}
+
+	@Override
+	public Void visitExpressionStmt(Stmt.Expression stmt) {
+		evaluate(stmt.expression);
+		return null;
+	}
+
+	@Override
+	public Void visitPrintStmt(Stmt.Print stmt) {
+		Object value = evaluate(stmt.expression);
+		System.out.println(stringify(value));
+		return null;
+	}
+
 	@Override
 	public Object visitBinaryExpr(Expr.Binary expr) {
 		Object left = evaluate(expr.left);
@@ -124,14 +154,4 @@ public class Interpreter implements Expr.Visitor<Object> {
 		}
 		return null;
 	}
-
-	public void interpret(Expr expression) {
-		try {
-			Object value = evaluate(expression);
-			System.out.println(stringify(value));
-		} catch (RuntimeError error) {
-			Lox.runtimeError(error);
-		}
-	}
-
 }
--- JLox/src/Parser.java
+++ JLox/src/Parser.java
@@ -11,6 +11,7 @@
  */
 
 import java.util.List;
+import java.util.ArrayList;
 
 public class Parser {
 	private static class ParseError extends RuntimeException {
@@ -23,17 +24,36 @@ public class Parser {
 		this.tokens = tokens;
 	}
 
-	public Expr parse() {
-		try {
-			return expression();
-		} catch (ParseError error) {
-			return null;
+	List<Stmt> parse() {
+		List<Stmt> statements = new ArrayList<>();
+		while (!isAtEnd()) {
+			statements.add(statement());
 		}
+		return statements;
 	}
 
 	// expression -> equality
 	private Expr expression() {
 		return equality();
+	}
+
+	private Stmt statement() {
+		if (match(TokenType.PRINT)) {
+			return printStatement();
+		}
+		return expressionStatement();
+	}
+
+	private Stmt printStatement() {
+		Expr value = expression();
+		consume(TokenType.SEMICOLON, "Expect ';' after value.");
+		return new Stmt.Print(value);
+	}
+
+	private Stmt expressionStatement() {
+		Expr expr = expression();
+		consume(TokenType.SEMICOLON, "Expect ';' after value.");
+		return new Stmt.Expression(expr);
 	}
 
 	// equality -> comparison (("!=" | "==") comparison)*
