c59796d05ea957ae0cca8fff3cf2273121c1cd32
queszel <queszeledu@gmail.com>
2025-03-16 17:31:16 -0300

    feat: Add variables to interpreter and remove AstPrinter (no longer needed).

---
+++ JLox/src/Environment.java
@@ -0,0 +1,17 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class Environment {
+	private final Map<String, Object> values = new HashMap<>();
+
+	Object get(Token name) {
+		if (values.containsKey(name.lexeme)) {
+			return values.get(name.lexeme);
+		}
+		throw new RuntimeError(name, "Undefined variable '" + name.lexeme + "'.");
+	}
+
+	public void define(String name, Object value) {
+		values.put(name, value);
+	}
+}
--- JLox/src/Interpreter.java
+++ JLox/src/Interpreter.java
@@ -2,6 +2,8 @@ import java.util.List;
 
 public class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
 
+	private Environment environment = new Environment();
+
 	public void interpret(List<Stmt> statements) {
 		try {
 			for (Stmt statement : statements) {
@@ -28,6 +30,11 @@ public class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
 				return -(double) right;
 		}
 		return null;
+	}
+
+	@Override
+	public Object visitVariableExpr(Expr.Variable expr) {
+		return environment.get(expr.name);
 	}
 
 	private void checkNumberOperand(Token operator, Object operand) {
@@ -102,6 +109,16 @@ public class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
 	public Void visitPrintStmt(Stmt.Print stmt) {
 		Object value = evaluate(stmt.expression);
 		System.out.println(stringify(value));
+		return null;
+	}
+
+	@Override
+	public Void visitVarStmt(Stmt.Var stmt) {
+		Object value = null;
+		if (stmt.initializer != null) {
+			value = evaluate(stmt.initializer);
+		}
+		environment.define(stmt.name.lexeme, value);
 		return null;
 	}
 
