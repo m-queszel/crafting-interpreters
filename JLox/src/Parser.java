63a14699d68b2fcab4147c87a072f85fa45def40
queszel <queszeledu@gmail.com>
2025-03-16 18:39:24 -0300

    feat: Add scope by using blocks ('{' and '}')

--- JLox/src/Parser.java
+++ JLox/src/Parser.java
@@ -53,6 +53,9 @@ public class Parser {
 		if (match(TokenType.PRINT)) {
 			return printStatement();
 		}
+		if (match(TokenType.LEFT_BRACE)) {
+			return new Stmt.Block(block());
+		}
 		return expressionStatement();
 	}
 
@@ -77,6 +80,15 @@ public class Parser {
 		Expr expr = expression();
 		consume(TokenType.SEMICOLON, "Expect ';' after value.");
 		return new Stmt.Expression(expr);
+	}
+
+	private List<Stmt> block() {
+		List<Stmt> statements = new ArrayList<>();
+		while (!check(TokenType.RIGHT_BRACE) && !isAtEnd()) {
+			statements.add(declaration());
+		}
+		consume(TokenType.RIGHT_BRACE, "Expect '}' after block.");
+		return statements;
 	}
 
 	private Expr assignment() {
